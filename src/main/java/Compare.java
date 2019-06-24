65cb5e99093945592ad6a8c05042e3448a6f6d18
Roman Beskrovnyi <roman.beskrovnyy@gmail.com>
2019-06-24 16:52:24 +0300

    #1: Added image-comparison as a dependency to the project. (#2)
    * #1: Added image-comparison as a dependency to the project.

--- src/main/java/Compare.java
+++ src/main/java/Compare.java
@@ -1,46 +1,34 @@
+import com.github.romankh3.image.comparison.ImageComparison;
+import com.github.romankh3.image.comparison.ImageComparisonUtil;
+import com.github.romankh3.image.comparison.model.ComparisonResult;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-import comparison.ImageComparison;
-import utils.ComparisonResult;
-
-
-
+/**
+ * Class for creating compare images.
+ */
 public final class Compare {
 
-	public static void compareImages(String actual, String expected,String result) throws IOException {
-		
+    public static void compareImages(String actual, String expected, String result) throws IOException {
 
-				BufferedImage expectedImage = ImageIO.read(new File (expected));
-				BufferedImage actualImage = ImageIO.read(new File (actual));
+        BufferedImage expectedImage = ImageIO.read(new File(expected));
+        BufferedImage actualImage = ImageIO.read(new File(actual));
 
-				// where to save the result (leave null if you want to see the result in the UI)
-				File resultDestination = new File( result);
-				ImageComparison imageComparison = new ImageComparison( expectedImage, actualImage, resultDestination );
+        // where to save the result (leave null if you want to see the result in the UI)
+        File resultDestination = new File(result);
+        ImageComparison imageComparison = new ImageComparison(expectedImage, actualImage, resultDestination);
 
-				imageComparison.setThreshold(10);
-				imageComparison.setRectangleLineWidth(5);
-				imageComparison.setDestination(resultDestination);
+        imageComparison.setThreshold(10);
+        imageComparison.setRectangleLineWidth(5);
+        imageComparison.setDestination(resultDestination);
 
-				//After configuring the ImageComparison object, can be executed compare() method:
-				ComparisonResult comparisonResult = imageComparison.compareImages();
-				BufferedImage resultImage = comparisonResult.getResult();
-				System.out.println("Result is: "+comparisonResult.getComparisonState());
-				
-				
-				
-				 File dir = resultDestination.getParentFile();
-		        boolean dirExists = dir == null || dir.isDirectory() || dir.mkdirs();
-		        if (!dirExists) {
-		            throw new FileNotFoundException("Unable to create directory " + dir);
-		        }
-		        ImageIO.write(resultImage, "png", resultDestination);
+        //After configuring the ImageComparison object, can be executed compare() method:
+        //Destination was added, that's why result image would be saved to resultDestination.
+        ComparisonResult comparisonResult = imageComparison.compareImages();
+        System.out.println("Result is: " + comparisonResult.getComparisonState());
+    }
 
-		
-	}
-	
-	
+
 }
--- src/main/java/comparison/IOConsumer.java
+++
@@ -1,9 +0,0 @@
-package comparison;
-
-import java.io.IOException;
-
-@FunctionalInterface
-interface IOConsumer<T> {
-
-    void accept(T t) throws IOException;
-}
